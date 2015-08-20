02d753bdabcda27579bdb9e85e5abfab1dab1dc9
MMxM <mmedeiros450@gmail.com>
2015-08-20 17:16:33 -0300

    updating URL_REGEX macro

--- src/kadimus_regex.h
+++ src/kadimus_regex.h
@@ -6,7 +6,7 @@
 #include "kadimus_common.h"
 
 #define VET_SIZE 30
-#define URL_REGEX "^.+\\/.*\\?.+\\=.+$"
+#define URL_REGEX "^.+\\/.*\\?.+\\=.*$"
 
 char **regex_extract(const char *regex, const char *data, int size_data, int Options, int *len);
 int regex_match(const char *regex, const char *data, int data_size, int Options);
