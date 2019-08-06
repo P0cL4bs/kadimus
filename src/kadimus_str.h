9ca16769392dd65b4a0df686a02ed9f5d94b3579
Matheus <mmedeiros450@gmail.com>
2019-08-05 23:51:18 -0300

    remove trash code

--- src/kadimus_str.h
+++ src/kadimus_str.h
@@ -11,35 +11,14 @@
 #define M_ALL_SIZE (R_SIZE-1)*2+5
 #define VULN_SIZE (R_SIZE-1)*2+11
 
-struct parameter_list {
-    size_t len;
-    struct parameter *parameter;
-    void *trash;
-};
-
-struct parameter {
-    char *key;
-    int key_size;
-    char *value;
-    int value_size;
-};
-
-struct dynptr {
-    char *ptr;
-    size_t len;
-};
-
 enum {
     replace_string,
     append_after,
     append_before
 };
 
-void extract_url(const char *url, char **base_uri, char **parameters);
 char *make_code(const char *mark, const char *code, bool auth);
-char *build_url(const char *base, struct parameter_list *plist, int pos, const char *new, int action);
 char *build_url_simple(const char *url, const char *parameter, const char *newstring, int opt);
 int parameter_exists(const char *url, const char *parameter);
-void tokenize(const char *parameters, struct parameter_list *plist);
 
 #endif
