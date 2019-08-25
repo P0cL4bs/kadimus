8c1fa820af5ededa7b56d5d64b2f4243ca08dfe7
Matheus <mmedeiros450@gmail.com>
2019-08-24 23:31:02 -0300

    update utils.h

--- src/io/utils.h
+++ src/io/utils.h
@@ -5,6 +5,15 @@
 #include <sys/poll.h>
 #include <stdio.h>
 
+#define foreach(line, fh) while(((line).nread = \
+    getline(&(line).buf, &(line).len, fh)) != -1)
+
+typedef struct {
+    ssize_t nread;
+    size_t len;
+    char *buf;
+} line_t;
+
 FILE *xfopen(const char *file, const char *mode);
 FILE *randomfile(char *filename, int retry);
 off_t getfdsize(int fd);
