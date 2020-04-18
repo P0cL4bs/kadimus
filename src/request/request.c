87bc759ba398e6dbdfaf96757e23b80e0ad59ca1
Matheus <mmedeiros450@gmail.com>
2020-04-18 00:25:33 -0300

    update request.c

--- src/request/request.c
+++ src/request/request.c
@@ -8,58 +8,78 @@
 size_t cbwrite(char *data, size_t size, size_t nmemb, body_t *body);
 
 void request_init(request_t *request){
-    CURL *curl;
+	CURL *curl;
 
-    memset(request, 0x0, sizeof(request_t));
+	memset(request, 0x0, sizeof(request_t));
 
-    curl = request->ch = curl_easy_init();
-    if(!curl)
-        die("curl_easy_init() error\n");
+	curl = request->ch = curl_easy_init();
+	if(!curl)
+		die("curl_easy_init() error\n");
 
-    curl_easy_setopt(curl, CURLOPT_USERAGENT, global.useragent);
-    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
-    curl_easy_setopt(curl, CURLOPT_TIMEOUT, global.timeout);
-    curl_easy_setopt(curl, CURLOPT_PROXY, global.proxy);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_easy_setopt(curl, CURLOPT_COOKIE, global.cookies);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &(request->body));
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, cbwrite);
+	curl_easy_setopt(curl, CURLOPT_USERAGENT, global.useragent);
+	curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
+	curl_easy_setopt(curl, CURLOPT_TIMEOUT, global.timeout);
+	curl_easy_setopt(curl, CURLOPT_PROXY, global.proxy);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_easy_setopt(curl, CURLOPT_COOKIE, global.cookies);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &(request->body));
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, cbwrite);
 
-    xmalloc(request->body.ptr, 1);
+	xmalloc(request->body.ptr, 1);
+}
+
+void request_init_fh(request_t *request)
+{
+	CURL *curl;
+
+	memset(request, 0x0, sizeof(request_t));
+
+	curl = request->ch = curl_easy_init();
+	if (!curl)
+		die("curl_easy_init() error\n");
+
+	curl_easy_setopt(curl, CURLOPT_USERAGENT, global.useragent);
+	curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
+	curl_easy_setopt(curl, CURLOPT_TIMEOUT, global.timeout);
+	curl_easy_setopt(curl, CURLOPT_PROXY, global.proxy);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_easy_setopt(curl, CURLOPT_COOKIE, global.cookies);
+
 }
 
 int request_exec(request_t *request){
-    CURLcode res;
-    size_t i;
+	CURLcode res;
 
-    for(i = 0; i <= global.retry; i++){
-        res = curl_easy_perform(request->ch);
+	for (size_t i = 0; i <= global.retry; i++) {
+		res = curl_easy_perform(request->ch);
 
-        if(res != CURLE_OK){
-            xerror("request failed: %s\n", curl_easy_strerror(res));
-            continue;
-        }
+		if (res != CURLE_OK) {
+			xerror("request failed: %s\n", curl_easy_strerror(res));
+			continue;
+		}
 
-        request->body.ptr[request->body.len] = 0x0;
+		if (request->body.ptr)
+			request->body.ptr[request->body.len] = 0x0;
 
-        return 0;
-    }
+		return 0;
+	}
 
-    return 1;
+	return 1;
 }
 
 void request_free(request_t *request){
-    curl_easy_cleanup(request->ch);
-    free(request->body.ptr);
+	curl_easy_cleanup(request->ch);
+	free(request->body.ptr);
 }
 
 size_t cbwrite(char *data, size_t size, size_t nmemb, body_t *body){
-    size_t newsize = body->len + size * nmemb;
+	size_t newsize = body->len + size * nmemb;
 
-    xrealloc(body->ptr, body->ptr, newsize + 1);
-    memcpy(body->ptr + body->len, data, size * nmemb);
-    body->len = newsize;
+	xrealloc(body->ptr, body->ptr, newsize + 1);
+	memcpy(body->ptr + body->len, data, size * nmemb);
+	body->len = newsize;
 
-    return size * nmemb;
+	return size * nmemb;
 }
--- src/request/request.h
+++ src/request/request.h
@@ -4,25 +4,26 @@
 #include <curl/curl.h>
 
 #define request_resetbody(x) do { \
-    free((x).body.ptr); \
-    (x).body.ptr = NULL; \
-    (x).body.len = 0; \
+	free((x).body.ptr); \
+	(x).body.ptr = NULL; \
+	(x).body.len = 0; \
 } while(0)
 
 typedef struct {
-    char *ptr;
-    size_t len;
+	char *ptr;
+	size_t len;
 } body_t;
 
 typedef struct {
-    int status;
-    char *filename;
-    CURL *ch;
-    body_t body;
-    int finish;
+	int status;
+	char *filename;
+	CURL *ch;
+	body_t body;
+	int finish;
 } request_t;
 
 void request_init(request_t *request);
+void request_init_fh(request_t *request);
 int request_exec(request_t *request);
 void request_free(request_t *request);
 
